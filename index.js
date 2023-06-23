18ff8bb66521da4caf3d1084c8719122eb619540
PeytenBevill <redsage00@gmail.com>
2023-06-22 21:01:30 -0500

    first on Peyten

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const pool = require('./sql/connections')
+const actorRoutes = require('./routes/R-actors')
+const movieRoutes = require('./routes/R-movies')
+
+
+const PORT = process.env.PORT || 8888
+
+app.listen(PORT, () => {
+  console.log('connected to the port')
+})
+
+app.use(express.json())
+// app.use('/actors', actorRoutes)
+app.use('/movies', movieRoutes)
+
+app.get('/', (req, res) => {
+  res.send('Welcome to our API!')
+})
